9ff2faddd41a30d3b8ac29f182aab4a9e2be8f59
Hannah Yoo <hyoo2@uw.edu>
2014-11-25 12:05:49 -0800

    Class Changes

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,27 @@
 
 'use strict';
 
+var http = require('http');
+
+//// req = requested object
+//// res = respond object
+//var server = http.createServer(function(req, res) {
+//    // 200 = all okay
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('You requested ' + req.url);
+//});
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World from Hannah!');
+});
+
+app.get('/foo', function(req, res) {
+    res.send('You requested /foo!');
+});
+
+app.listen(8080, function() {
+    console.log('Server listening on http://localhost:8080');
+});
